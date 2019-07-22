e5277cc6be532949fabf0f9c0ae9291fe5bde644
Vincent Esche <regexident@gmail.com>
2019-07-22 12:34:48 +0200

    Added basic `BayesFilter` protocol

--- Sources/BayesFilter/BayesFilter.swift
+++ Sources/BayesFilter/BayesFilter.swift
@@ -1,3 +1,30 @@
-struct BayesFilter {
-    var text = "Hello, World!"
+public protocol BayesFilter {
+    associatedtype Observation
+    associatedtype Control
+    associatedtype Prediction
+    associatedtype Estimate
+
+    func predict(
+        control: Control
+    ) -> Prediction
+
+    func update(
+        prediction: Prediction,
+        observation: Observation,
+        control: Control
+    ) -> Estimate
+}
+
+extension BayesFilter {
+    public func filter(
+        observation: Observation,
+        control: Control
+    ) -> Estimate {
+        let prediction = self.predict(control: control)
+        return self.update(
+            prediction: prediction,
+            observation: observation,
+            control: control
+        )
+    }
 }
