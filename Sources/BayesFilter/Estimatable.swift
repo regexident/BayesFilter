ebc7ba41a450405ca79ebadd5c5ca8416c4938e3
Vincent Esche <regexident@gmail.com>
2019-10-21 20:24:59 +0200

    Deleted file 'EstimateAccessible.swift'

--- Sources/BayesFilter/Estimatable.swift
+++ Sources/BayesFilter/Estimatable.swift
@@ -1,3 +1,13 @@
+import StateSpace
+
 public protocol Estimatable {
     associatedtype Estimate
+}
+
+public protocol EstimateReadable: Estimatable {
+    var estimate: Estimate { get }
+}
+
+public protocol EstimateReadWritable: EstimateReadable {
+    var estimate: Estimate { get set }
 }
--- Sources/BayesFilter/EstimateAccessible.swift
+++
@@ -1,9 +0,0 @@
-import StateSpace
-
-public protocol EstimateReadable: Estimatable {
-    var estimate: Estimate { get }
-}
-
-public protocol EstimateReadWritable: EstimateReadable {
-    var estimate: Estimate { get set }
-}
