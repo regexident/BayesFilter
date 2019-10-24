a2b674767c326ef6f3af3e742d24b48ebf701f01
Vincent Esche <regexident@gmail.com>
2019-10-24 16:38:58 +0200

    Added `func batchPredicted(…)` & `func batchPredict(…)` to `protocol BayesPredictorProtocol`

--- Sources/BayesFilter/BayesPredictorProtocol.swift
+++ Sources/BayesFilter/BayesPredictorProtocol.swift
@@ -2,6 +2,24 @@ import StateSpace
 
 public protocol BayesPredictorProtocol: Estimatable {
     func predicted(estimate: Estimate) -> Estimate
+
+    func batchPredicted(
+        estimate: Estimate,
+        iterations: Int
+    ) -> Estimate
+}
+
+extension BayesPredictorProtocol {
+    public func batchPredicted(
+        estimate: Estimate,
+        iterations: Int
+    ) -> Estimate {
+        return (0..<iterations).reduce(estimate) { estimate, _ in
+            return self.predicted(
+                estimate: estimate
+            )
+        }
+    }
 }
 
 extension BayesPredictorProtocol
@@ -19,6 +37,27 @@ public protocol ControllableBayesPredictorProtocol: Estimatable, Controllable {
         estimate: Estimate,
         control: Control
     ) -> Estimate
+
+    func batchPredicted<S>(
+        estimate: Estimate,
+        controls: S
+    ) -> Estimate where S: Sequence, S.Element == Control
+}
+
+extension ControllableBayesPredictorProtocol {
+    public func batchPredicted<S>(
+        estimate: Estimate,
+        controls: S
+    ) -> Estimate
+        where S: Sequence, S.Element == Control
+    {
+        return controls.reduce(estimate) { estimate, control in
+            return self.predicted(
+                estimate: estimate,
+                control: control
+            )
+        }
+    }
 }
 
 extension ControllableBayesPredictorProtocol
@@ -28,6 +67,17 @@ extension ControllableBayesPredictorProtocol
         self.estimate = self.predicted(
             estimate: self.estimate,
             control: control
+        )
+    }
+
+    public mutating func batchPredict<S>(
+        controls: S
+    )
+        where S: Sequence, S.Element == Control
+    {
+        self.estimate = self.batchPredicted(
+            estimate: self.estimate,
+            controls: controls
         )
     }
 }
